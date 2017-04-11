066a9b9b6e81cfe914343c110e59c7236135e39c
Your Name <LJAndrea@D-108-179-174-242.dhcp4.washington.edu>
2017-04-11 14:42:26 -0700

    finished exercise 3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -15,37 +15,42 @@ data.frame('USPersonalExpenditure')
 US.Personal.Expenditure <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-#X1940, X1945, X1950, X1955, 
+colnames(US.Personal.Expenditure)
 
 # Why are they so strange?
-# Maybe it refers to some sort of axis - like X and Y axes for 2D data
+# Can't store columns names w/ just numbers
 
 # What are the row names of your dataframe?
-# Food and Tobacco, Household Operation, Medical and Health, Personal Care, Private Education 
+rownames(US.Personal.Expenditure)
 
 # Create a column `category` that is equal to your rownames
 US.Personal.Expenditure$category <- rownames(US.Personal.Expenditure)
 
 # How much money was spent on personal care in 1940?
-# 1.040
+care.1940 <- US.Personal.Expenditure['Personal Care', 'X1940']
 
 # How much money was spent on Food and Tobacco in 1960
 # 5.40
 
 # What was the highest expenditure category in 1960?
-# Food and Tobacco
+highest.1960 <- US.Personal.Expenditure[US.Personal.Expenditure$X1960 == 
+                                          max(US.Personal.Expenditure$X1960), 'category']
 
 ### Bonus ###
 
 # Write a function that takes in a year as a parameter, and 
-# returns the highest spending category of that year
+# returns the highest spending category of that yearsf
 
 HighestSpendingCategory <- function(year) {
-  year.info <- 
+  return (US.Personal.Expenditure$category[US.Personal.Expenditure[year] == 
+                                             max(US.Personal.Expenditure[year])])
 }
 
 # Using your function, determine the highest spending category of each year
-
+highest.1955 <- HighestSpendingCategory('X1955')
+highest.1950 <- HighestSpendingCategory('X1950')
+highest.1945 <- HighestSpendingCategory('X1945')
+highest.1940 <- HighestSpendingCategory('X1940')
 
 # Write a loop to cycle through the years, and store the highest spending category of
 # each year in a list
